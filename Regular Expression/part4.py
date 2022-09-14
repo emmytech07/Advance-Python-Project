d8a3829c24625e18ea81baee39cc7c12fe3f8a2c
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-14 05:38:21 +0100

    part 4 for regular expression

---
+++ Regular Expression/part4.py
@@ -0,0 +1,19 @@
+import re 
+
+#  | Alternation (Find either the value)
+# match_result = re.finditer("a|bd", "Adachtbd")
+
+# for match in match_result:
+#     print(match)
+
+#  | Group (looking for any of letter but should end with outer value)
+# match_result = re.finditer("(a|bd)xz", "Adaxzchtbdxz")
+
+# for match in match_result:
+#     print(match)
+
+#  \ Backslash (to escape and becoming neutral)
+match_result = re.finditer("\^xy", "^xy")
+
+for match in match_result:
+    print(match)
